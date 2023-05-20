c1b2ac69a9c65bed153ba9e529a5e0402c3149d5
Tanvi Deepak Kapote <90285650+Tanv08@users.noreply.github.com>
2023-05-19 20:32:57 -0400

    Add files via upload

---
+++ Order Type table.sql
@@ -0,0 +1,4 @@
+insert into order_type values ('1','Online');
+insert into order_type values ('2','Counter');
+insert into order_type values ('3','Phone');
+ Select * from Order_Type
---
+++ Sales Person Table.sql
@@ -0,0 +1,5 @@
+insert into sales_person values('11','Amir','Woods');				
+insert into sales_person values('12','Megan','Boyer');				
+insert into sales_person values('13','Lorelei','Tapia');				
+insert into sales_person values('14','Elise','Gallegos');				
+insert into sales_person values('15','Alaina','Huynh');				
